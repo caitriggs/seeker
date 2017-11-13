b6e2c4822fcff146102a3cf53ae9bf1c82b627cc
Cait <caitriggs@gmail.com>
2017-11-13 14:07:59 -0800

    Update notebook to use get_data module and start setting up mongoDB

--- src/get_data.py
+++ src/get_data.py
@@ -3,30 +3,36 @@ import os
 
 class BungieData(object):
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, membershipType):
         '''
         api_key (str): The api key given to you by Bungie when you registered your app with them
+        membershipType (int): The console/network the player's account is on (i.e. 1=Xbox, 2=PSN)
         '''
         self.api_key = api_key
+        self.membershipType = membershipType
+        self.membership_id = 0 #call get_DestinyUserId to find player's Destiny Id
 
     def get_playerByTagName(self, gamertag):
-        '''gamertag (str): The PSN gamertag a player uses on Destiny 2'''
-        site_call = "https://bungie.net/Platform/Destiny2/SearchDestinyPlayer/2/" + gamertag
+        '''gamertag (str): The gamertag a player uses on Destiny 2'''
+        site_call = "https://bungie.net/Platform/Destiny2/SearchDestinyPlayer/" \
+            + str(self.membershipType) + "/" + gamertag
         request = requests.get(site_call,
-                                headers={"X-API-Key":self.api_key})
+                               headers={"X-API-Key":self.api_key})
         return request.json()['Response']
 
     def get_DestinyUserId(self, gamertag):
-        '''gamertag (str): The PSN gamertag a player uses on Destiny 2'''
+        '''gamertag (str): The gamertag a player uses on Destiny 2'''
         info = self.get_playerByTagName(gamertag)
-        return int(info[0]['membershipId'])
+        self.membership_id = int(info[0]['membershipId'])
+        return self.membership_id
 
     def get_BungieUserId(self, membership_id):
         '''
         membership_id (int): the Destiny membership_id of a player (the id returned by get_DestinyUserId)
         Uses old Destiny endpoint for a PSN user to get the BUNGIE membershipId
         '''
-        site_call = "https://bungie.net/Platform/User/GetMembershipsById/" + str(membership_id) + "/2/"
+        site_call = "https://bungie.net/Platform/User/GetMembershipsById/" \
+            + str(membership_id) + "/" + str(self.membershipType)
         request = requests.get(site_call,
                                 headers={"X-API-Key":self.api_key})
         return int(request.json()['Response']['bungieNetUser']['membershipId'])
@@ -39,7 +45,8 @@ class BungieData(object):
         Uses new Destiny 2 endpoint for PSN player using the Destiny membershipId
         '''
         components = "?components=" + ','.join([str(c) for c in components])
-        site_call = "https://bungie.net/Platform/Destiny2/2/Profile/" + str(membership_id) + "/" + components
+        site_call = "https://bungie.net/Platform/Destiny2/" + str(self.membershipType) + "/Profile/" \
+            + str(membership_id) + "/" + components
         request = requests.get(site_call,
                                 headers={"X-API-Key":self.api_key})
         return request.json()['Response']
@@ -51,6 +58,14 @@ class BungieData(object):
                                 headers={"X-API-Key":self.api_key})
         return request.json()['Response']
 
+    def get_CharacterIdsByPlayer(self, membership_id):
+        user_data = self.get_DestinyUserProfile(membership_id, components=[100])
+        return user_data['profile']['data']['characterIds']
+
+    def get_activitiesByCharacter(self, character_id):
+        site_call = "https://bungie.net/Platform/Destiny2/" + str(self.membershipType) + "/Profile/" \
+            + str(membership_id) + "/"
+
     def get_Manifest(self):
         site_call = "https://bungie.net/Platform/Destiny2/Manifest/"
         request = requests.get(site_call,
@@ -58,7 +73,8 @@ class BungieData(object):
         return request.json()['Response']
 
     def get_PlayerStats(self, membership_id):
-        site_call = "https://bungie.net/Platform/Destiny2/2/Account/" + str(membership_id) + "/Stats/"
+        site_call = "https://bungie.net/Platform/Destiny2/" + str(self.membershipType) + \
+            "/Account/" + str(membership_id) + "/Stats/"
         request = requests.get(site_call,
                                 headers={"X-API-Key":self.api_key})
         return request.json()['Response']
@@ -71,7 +87,7 @@ class BungieData(object):
 
 
 if __name__ == '__main__':
-    bungie = BungieData(api_key=os.environ["BUNGIE_API_KEY"]) # Never put your keys in code... export 'em!
+    bungie = BungieData(api_key=os.environ["BUNGIE_API_KEY"], membershipType=2) # Never put your keys in code... export 'em!
 
     # Get Destiny MembershipId by PSN gamertag
     my_destiny_id = bungie.get_DestinyUserId("BalancedSeeker6")
